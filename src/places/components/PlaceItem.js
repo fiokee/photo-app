66afeff243fa5d342c64a7404f19440dfdb27519
fiokee <smleffiong@gmail.com>
2023-10-02 18:25:31 +0100

    create dynamic modal component and button component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
 import './PlaceItem.css';
 import Card from '../../shared/UiElement/Card/Card';
+import Button from '../../shared/formElement/Button/Button';
+import Modal from '../../shared/UiElement/Modal/Modal';
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap]= useState(false);
+
+  const showMapHandler = ()=> setShowMap(true);
+
+  const closeMapHandler = ()=> setShowMap(false);
+
   return (
+    <Fragment>
+      <Modal
+       show={showMap}
+        onCancel={closeMapHandler} 
+        header={props.title} 
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+        >
+          <div className='map-container'>
+            <h2>The Map</h2>
+          </div>
+        </Modal>
+
     <li className='place-item'>
       <Card className='place-item__content'>
       <div className='place-item__image'>
@@ -15,12 +37,13 @@ const PlaceItem = (props) => {
         <p>{props.description}</p>
       </div>
       <div className='place-item__actions'>
-        <button>VIEW ON MAP</button>
-        <button>EDIT</button>
-        <button>DELETE</button>
+        <Button inverse onClick={showMapHandler}>VIEW ON MAP</Button>
+        <Button to={`/places/${props.id}`}>EDIT</Button>
+        <Button danger>DELETE</Button>
       </div>
       </Card>
     </li>
+    </Fragment>
   )
 }
 
